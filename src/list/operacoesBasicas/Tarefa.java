5837ef794424946d6f417c7f96dc3500706a268f
Yuri Vasques <yurivasques@gmail.com>
2024-08-29 17:52:31 -0300

    exercicio lista de Tarefas

---
+++ src/list/operacoesBasicas/Tarefa.java
@@ -0,0 +1,16 @@
+package list.operacoesBasicas;
+
+public class Tarefa {
+
+    private String descricao;
+
+    public Tarefa(String descricao){
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    
+}
